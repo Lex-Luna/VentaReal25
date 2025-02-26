3e98614fd4e950bcffd62724a45837d6bed7f9b8
Lex Luna <alexisgabriel.lu@gmail.com>
2025-02-26 22:40:28 +0100

    componente home

--- Ventas/src/app/app.routes.ts
+++ Ventas/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from '../component/home/home.component';
+// ... existing code ...
+export const routes: Routes = [
+    { path: "home", component: HomeComponent },
+    { path: "", redirectTo: 'home', pathMatch: "full" }
+];
