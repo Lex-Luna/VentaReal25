d50d37b286b0c706ac56356e0bd3b03a756ea73f
Lex Luna <alexisgabriel.lu@gmail.com>
2025-02-26 23:19:57 +0100

    add libreria material & routing

--- Ventas/src/app/app.component.ts
+++ Ventas/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterModule, MatSidenavModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ Ventas/src/component/cliente/cliente.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+@Component({
+  selector: 'app-cliente',
+  imports: [MatSidenavModule],
+  templateUrl: './cliente.component.html',
+  styleUrl: './cliente.component.scss'
+  
+})
+export class ClienteComponent {
+
+}
