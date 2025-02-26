5af8b923d7c4f231c5f2fe79d529eeb2c43bc865
Lex Luna <alexisgabriel.lu@gmail.com>
2025-02-26 22:49:46 +0100

    componente cliente

--- Ventas/src/app/app.routes.ts
+++ Ventas/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 
 import { HomeComponent } from '../component/home/home.component';
+import { ClienteComponent } from '../component/cliente/cliente.component';
 // ... existing code ...
 export const routes: Routes = [
     { path: "home", component: HomeComponent },
-    { path: "", redirectTo: 'home', pathMatch: "full" }
+    { path: "", redirectTo: 'home', pathMatch: "full" },
+    { path: "cliente", component: ClienteComponent }
 ];
